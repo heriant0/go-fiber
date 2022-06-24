793fbe2ddc19e089e76ba21d3765bd47397640e9
herianto <herianto890@gmail.com>
2022-06-25 05:22:27 +0700

    setup fiber

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/heriant0/go-fiber/configs"
+	"github.com/heriant0/go-fiber/models"
+)
+
 func main() {
-	println("go module init")
+	db := configs.Load()
+	app := fiber.New()
+
+	db.AutoMigrate(&models.Book{})
+
+	db.Create(&models.Book{ID: "A001", Title: "Golang", Author: "google team"})
+
+	var books models.Book
+	findById := db.First(&books, "id = ?", "A001")
+	println(findById)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
